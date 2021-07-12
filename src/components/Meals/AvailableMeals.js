df6e8eb8fbdaa34359e370a8fadb05f01227e40a
WarisAli-source <warissid41@gmail.com>
2021-07-12 23:25:03 +0530

    finished

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -5,25 +5,25 @@ import classes from './AvailableMeals.module.css';
 const DUMMY_MEALS = [
   {
     id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
+    name: 'Hyderabadi Biryani',
+    description: 'A chicken you will fall for',
     price: 22.99,
   },
   {
     id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
+    name: 'Pizza',
+    description: 'Fresh out of the oven.',
     price: 16.5,
   },
   {
     id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
+    name: 'Burger',
+    description: 'Snacks time made better.',
     price: 12.99,
   },
   {
     id: 'm4',
-    name: 'Green Bowl',
+    name: 'Rice Bowl',
     description: 'Healthy...and green...',
     price: 18.99,
   },
